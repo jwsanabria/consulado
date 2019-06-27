13ae6ec8b58a6009fe071becfdaf1632ac463abe
Ricardo <alejo0920@gmail.com>
2019-06-27 16:29:44 -0500

    MIC-235 Automatización de generación de backup

--- src/main/docker/getmesh/backup.sh
+++ src/main/docker/getmesh/backup.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env sh
 
-BASE_URL="localhost:8005"
+BASE_URL="localhost:8080"
 MESH_ADMIN_USER="admin"
 MESH_ADMIN_PASSWORD="admin"
+CONTAINER_ID=$(docker ps | grep 'miconsulado/getmesh:latest' | awk '{ print $1 }')
+echo "CONTAINER:"${CONTAINER_ID}
 
+curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
+curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/backup"
 
-curl -c /mesh/cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
-curl -X POST -b /mesh/cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/backup"
-
-docker cp ${CONTAINER_ID}":/backups/* ./backup/graphdb/"
-docker cp ${CONTAINER_ID}":/uploads/ ./backup/uploads/"
+docker cp ${CONTAINER_ID}":/backups/*" "./backup/graphdb/"
+docker cp ${CONTAINER_ID}":/uploads/" "./backup/uploads/"
+rm ./cookies.txt
 
