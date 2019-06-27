2237174aa612c3c28c98e4dba71017be82219de1
root <root@vps206188.vps.ovh.ca>
2019-06-27 17:50:26 -0400

    Ajustes para automatización de backup y restauración

--- src/main/docker/getmesh/backup.sh
+++ src/main/docker/getmesh/backup.sh
@@ -6,10 +6,11 @@ MESH_ADMIN_PASSWORD="admin"
 CONTAINER_ID=$(docker ps | grep 'miconsulado/getmesh:latest' | awk '{ print $1 }')
 echo "CONTAINER:"${CONTAINER_ID}
 
-curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
-curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/backup"
+#curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
+#curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/backup"
 
-docker cp ${CONTAINER_ID}":/backups/*" "./backup/graphdb/"
-docker cp ${CONTAINER_ID}":/uploads/" "./backup/uploads/"
+rm -rf ./backup/*
+docker cp ${CONTAINER_ID}":/backups" "./backup/"
+docker cp ${CONTAINER_ID}":/uploads" "./backup/"
 rm ./cookies.txt
 
