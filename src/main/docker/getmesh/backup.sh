33ddbf3fab5b13f76f389bc6d7bd06bde50687b3
Ricardo Alejo <alejo0920@gmail.com>
2019-06-30 15:52:11 +0000

    Ajustes a atutomatización de restauración de backup

--- src/main/docker/getmesh/backup.sh
+++ src/main/docker/getmesh/backup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-BASE_URL="localhost:8080"
+BASE_URL="localhost:8005"
 MESH_ADMIN_USER="admin"
 MESH_ADMIN_PASSWORD="admin"
-CONTAINER_ID=$(docker ps | grep 'miconsulado/getmesh:latest' | awk '{ print $1 }')
+CONTAINER_ID=$(docker ps | grep 'getmesh' | awk '{ print $1 }')
 echo "CONTAINER:"${CONTAINER_ID}
 
 curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
--- src/main/docker/getmesh/restore.sh
+++ src/main/docker/getmesh/restore.sh
@@ -8,11 +8,13 @@ MESH_ADMIN_PASSWORD="admin"
 
 
 BASE_URL="localhost:8005"
-CONTAINER_ID=$(docker ps | grep 'miconsulado/getmesh:latest' | awk '{ print $1 }')
+CONTAINER_ID=$(docker ps | grep 'getmesh' | awk '{ print $1 }')
 
-curl -c /mesh/cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
-curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/restore"
-docker restart CONTAINER_ID
+curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
+curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/restore" 
+docker restart ${CONTAINER_ID} &
+echo "RESTARTING..."
 sleep ${MESH_START_DELAY}
-curl -c /mesh/cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
+echo "RESTARTED..."
+curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
 curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/search/sync"
