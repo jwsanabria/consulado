5aa62215d911fc53253808a0440981c05e3c650a
Ricardo <alejo0920@gmail.com>
2019-07-11 19:54:59 -0500

    MIC-265 Restauración backup desde entrypoint

--- src/main/docker/getmesh/backup.sh
+++ src/main/docker/getmesh/backup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-BASE_URL="localhost:8005"
+BASE_URL="localhost:8080"
 MESH_ADMIN_USER="admin"
 MESH_ADMIN_PASSWORD="admin"
-CONTAINER_ID=$(docker ps | grep 'getmesh' | awk '{ print $1 }')
+CONTAINER_ID=$(docker ps | grep 'miconsulado/getmesh:latest' | awk '{ print $1 }')
 echo "CONTAINER:"${CONTAINER_ID}
 
-curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
-curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/backup"
+#curl -c ./cookies.txt ${MESH_ADMIN_USER}":"${MESH_ADMIN_PASSWORD}"@"${BASE_URL}"/api/v2/auth/login"
+#curl -X POST -b ./cookies.txt ${BASE_URL}"/api/v2/admin/graphdb/backup"
 
 rm -rf ./backup/*
 docker cp ${CONTAINER_ID}":/backups" "./backup/"
---
+++ src/main/docker/getmesh/bin/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+/mesh/bin/restore.sh &
+
+cd /mesh
+java -jar mesh.jar
+
+
